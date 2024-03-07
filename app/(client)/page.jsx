6d3ee35912a66ad04a3baf0ba1338f057927c949
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-03-06 18:01:01 -0800

    Added all tags to main page

--- app/(client)/page.jsx
+++ app/(client)/page.jsx
@@ -3,22 +3,54 @@ import Link from "next/link";
 
 import { getAllPosts } from "@actions/get-all-posts";
 import { getFeaturedPosts } from "@actions/get-featured-posts";
+import { getAllTags } from "@actions/get-all-tags";
 
 // Dynamic imports
-import { HeroBanner, Header, Separator, PostCard } from "@components/index";
+import {
+  HeroBanner,
+  Header,
+  Separator,
+  PostCard,
+  TagCard,
+} from "@components/index";
 
 export const revalidate = 15;
 
 export default async function Home() {
   const posts = await getAllPosts();
   const featuredPosts = await getFeaturedPosts();
+  const tags = await getAllTags();
 
   return (
     <div className="flex w-full flex-col">
       <HeroBanner />
 
-      {/* FEATURED POSTS CONTAINER */}
+      {/* ALL TAGS CONTAINER */}
       <div id="main" className="container flex w-full flex-col pt-20">
+        <Header title="All Tags" />
+        <Separator />
+
+        <div className="flex flex-wrap justify-around gap-5">
+          {tags?.length > 0 &&
+            tags?.map((tag) => {
+              return <TagCard isTagCount key={tag._id} tag={tag} />;
+            })}
+        </div>
+
+        <div className="mt-10 flex items-center justify-center">
+          <Link href="/tags">
+            <Button
+              size="lg"
+              className=" transform rounded-3xl bg-myDarkBlue text-myLightGreyBlue transition duration-200 hover:scale-105 hover:text-slate-200 dark:bg-myLightGreyBlue dark:text-myDarkBlue hover:dark:text-slate-900"
+            >
+              View All Tags
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      {/* FEATURED POSTS CONTAINER */}
+      <div className="container flex w-full flex-col pt-20">
         <Header title="Featured Posts" />
         <Separator />
         <div className="flex flex-wrap justify-center gap-10">
@@ -40,7 +72,7 @@ export default async function Home() {
       </div>
 
       {/* RECENT POSTS CONTAINER */}
-      <div id="main" className="container flex w-full flex-col pt-20">
+      <div className="container flex w-full flex-col pt-20">
         <Header title="Recent Posts" />
         <Separator />
         <div className="flex flex-wrap justify-center gap-10">
