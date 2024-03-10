4ce56a39edfd886f6c02f7dbd9fdafcbe7a5b88d
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-03-09 17:42:38 -0800

    Changed styling of table of contents and scroll to top button

--- components/Post/TableOfContents.jsx
+++ components/Post/TableOfContents.jsx
@@ -2,14 +2,15 @@ import { slugify } from "@utils/helpers";
 
 const TableOfContents = ({ headings }) => {
   return (
-    <div className="rounded-sm border-2 border-mySteelBlue">
-      <h2 className="border-b-2 border-mySteelBlue px-10 py-2 text-2xl font-bold">
+    <div className="rounded-sm border-2 border-myLightGreyBlue bg-slate-800 text-myLightGreyBlue dark:border-slate-800 dark:bg-myLightGreyBlue dark:text-slate-800">
+      <h2 className="border-b-2 border-myLightGreyBlue px-10 py-2 text-2xl font-bold dark:border-slate-800">
         Table of Contents
       </h2>
-      <nav className="py-2 pl-10">
+      <nav className="py-2 pl-12">
         <ul>
           {headings?.map((heading) => (
             <li key={heading?._key} className="mb-2 text-lg font-semibold">
+              <span className="mr-2">-</span>
               <a
                 className="hover:underline"
                 href={`#${slugify(heading?.children[0]?.text)}`}
--- components/reuseable/ScrollToTopButton.jsx
+++ components/reuseable/ScrollToTopButton.jsx
@@ -15,7 +15,10 @@ const ScrollToTopButton = () => {
       } fixed bottom-10 right-10 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-primary-dark shadow-lg shadow-gray-900 transition-transform duration-200 dark:bg-primary-light 2xl:right-96`}
     >
       {/* svg icon here*/}
-      <ArrowUp strokeWidth={2.5} className="text-myImperialBlue" />
+      <ArrowUp
+        strokeWidth={2.5}
+        className="text-mySteelBlue dark:text-myImperialBlue"
+      />
     </button>
   );
 };
